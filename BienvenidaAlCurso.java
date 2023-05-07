81d6012814ac0cffd1c2fa66e175917c3e9e4052
Dan1elMon <dchuramo@unsa.edu.pe>
2023-05-07 10:26:45 -0500

    desarrollando BienvenidaAlCurso.java

---
+++ BienvenidaAlCurso.java
@@ -0,0 +1,7 @@
+public class BienvenidaAlCurso{
+    public static void main(String [] args){
+        String nombre = "Daniel";
+        System.out.println("Bienvenido al curso "+nombre +" :D");
+
+    }
+}
